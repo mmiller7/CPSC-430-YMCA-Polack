1e93febabb640e984a1e8f93f09d3f9bd1d54365
Matt <mmiller7@mail.umw.edu>
2011-11-25 12:08:01 -0500

    Added if-else case to test bluehost

--- project/dbconnect.php
+++ project/dbconnect.php
@@ -1,5 +1,15 @@
 <?php
-	$db = mysqli_connect('localhost', 'swimrayuser', 'swimrayuser','swimrays_db');
+	if($SERVER['HTTP_HOST'] == "swimrays.mwcpsc.org")
+	{
+		//For Bluehost
+		$db = mysqli_connect('localhost', 'mwcpscor_swimray', 'swimrayuser','mwcpscor_swimrays_db');
+	}
+	else
+	{
+		//For local testing
+		$db = mysqli_connect('localhost', 'swimrayuser', 'swimrayuser','swimrays_db');
+	}
+	
 	if (!$db){
 		die("Couldn't connect!");
 	}
